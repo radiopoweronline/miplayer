65492afdff353b15293c4587572fa9b2d12d1905
radiopoweronline <149813107+radiopoweronline@users.noreply.github.com>
2024-12-24 11:53:24 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,52 @@
+window.streams = {
+    timeRefresh: 10000,
+    stations: [
+        {
+            name: "Radio Power",
+            hash: "Radiopower",
+            description: "Con el poder de los clásicos",
+            logo: "assets1/cover.png",
+            album: "assets1/cover1.png",
+            cover: "assets1/cover.png",
+            api: "",
+            stream_url: "https://stream.zeno.fm/dlvtl3hthyxvv",
+            tv_url: "",
+            server: "",
+            apps: {
+                android: "",
+                ios: "",
+            },
+        },
+        {
+            name: "URBANA",
+            hash: "URBANA",
+            description: "",
+            logo: "assets1/logo.png",
+            album: "assets1/cover2.png",
+            cover: "assets1/cover2.png",
+            api: "https://api.zeno.fm/mounts/metadata/subscribe/cxf1r8zukyhuv",
+            stream_url: "https://stream.zeno.fm/cxf1r8zukyhuv",
+            tv_url: "",
+            server: "",
+            apps: {
+                android: "#",
+                ios: "#",
+            },
+        },
+        {
+            name: "SALSA",
+            hash: "SALSA",
+            description: "",
+            logo: "assets1/cover4.png",
+            album: "assets1/cover4.png",
+            cover: "assets1/logo.png",
+            api: "",
+            stream_url: "https://stream.zeno.fm/v99hvs8ojo3tv",
+            server: "spotify",
+            apps: {
+                android: "",
+                ios: "",
+            },
+        },
+    ],
+};
